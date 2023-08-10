00b6827f24a61bbdee0996296f9ff90d76c23368
Fernando Labandeira <nandolabandeira15@gmail.com>
2023-08-10 17:24:58 -0300

    creo en el arcivo eventos.js la sintaxis para crear el evento en el boton le agrego atributo style al elemento div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let saludar = document.getElementById("boton-saludar");
+    saludar.addEventListener("click", function(){
+        alert("Hola");
+    })
+})
