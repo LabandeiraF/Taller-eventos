3410d117a05fa3c32ff7e36d42e4d003abac446d
Fernando Labandeira <nandolabandeira15@gmail.com>
2023-08-10 19:01:09 -0300

    creo evento para parar el mensaje del div

--- eventos.js
+++ eventos.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     let saludar = document.getElementById("boton-saludar");
     saludar.addEventListener("click", function(){
+        Event.stopPropagation();
         alert("Hola");
     })
 
